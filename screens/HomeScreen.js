7e0fbde48edf5ec9692c00c937eed05dd617bb47
yusuf <vadapallimumtaj123@gmail.com>
2021-07-28 10:38:26 +0530

    offline

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { Header } from 'react-native-elements';
+import dictionary from './database';
+
 export default class HomeScreen extends Component{
   constructor() {
     super();
@@ -14,47 +16,25 @@ export default class HomeScreen extends Component{
     };
   }
 
-  getWord=(word)=>{
-    var searchKeyword=word.toLowerCase()
-    var url = "https://rupinwhitehatjr.github.io/dictionary/"+searchKeyword+".json"
-    //console.log(url)
-    return fetch(url)
-    .then((data)=>{
-      if(data.status===200)
-      {
-        return data.json()
-      }
-      else
-      {
-        return null
-      }
-    })
-    .then((response)=>{
-
-        var responseObject = response
-        if(responseObject)
-        {
-          var wordData = responseObject.definitions[0]
-          var definition=wordData.description
-          var lexicalCategory=wordData.wordtype
-          this.setState({
-            "word" : this.state.text, 
-            "definition" :definition,
-            "lexicalCategory": lexicalCategory     
-            
-          })
-        }
-        else
-        {
-          this.setState({
-            "word" : this.state.text, 
-            "definition" :"Not Found",
-            
-          })
-
-        }
-    
-    })
+  getWord=(text)=>{
+    var text =text.toLowerCase()
+    try{
+      var word = dictionary[text]["word"]
+      var lexicalCategory = dictionary[text]["lexicalCategory"]
+      var definition = dictionary[text]["defination"]
+      this.setState({
+        "word":word,
+        "lexicalCategory":lexicalCategory,
+        "defination":defination
+      })
+    }
+    catch(err){
+      alert("Sorry This Word Is Not Available")
+      this.setState({
+        'text':'',
+        'isSearchPressed':false
+      })
+    }
   }
 
   render(){
